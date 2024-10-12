6d00a546a7183ffc0aaf34409f92d6c77a2dd520
Navindu Kavishka <navindukavishka2001@gmail.com>
2024-10-12 12:03:39 +0530

    db config

--- config/db.js
+++ config/db.js
@@ -1,2 +1,23 @@
 const mysql = require("mysql");
 const dotenv = require("dotenv");
+dotenv.config();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    
+});
+
+db.connect(
+    (err) => {
+        if (err){
+            console.log("db connection is not working bro...",err);
+        } else {
+            console.log("db connection is working bro...");
+        }
+    }
+);
+
+module.exports = db;
